2f1470a84fce93171ffef12c36e30ab99cffc1a2
yoonyerin <yerinyoon@ewhain.net>
2022-10-09 13:12:09 +0900

    user_controller

---
+++ src/main/java/com/gamjaring/web/springboot/controller/UserForm.java
@@ -0,0 +1,43 @@
+package com.gamjaring.web.springboot.controller;
+
+public class UserForm {
+    private String name;
+    private String email;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    //gender는 binary로 가고 싶은데 방법 고민중
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+}
--- src/main/java/com/gamjaring/web/springboot/user/UserServiceImpl.java
+++ src/main/java/com/gamjaring/web/springboot/user/UserServiceImpl.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -34,11 +40,32 @@ public class UserServiceImpl implements UserService{
 
         }
 
+        ////고민해볼 파트////
         if(!passwordEncoder.matches(password, loginUser.getPassword())){
             System.out.println("비밀번호가 일치하지 않습니다.");
             return false;
-        }
+         }
+
         return true;
+    }
+
+
+
+    //private이어야 할지 고민해보기
+    public void validateDuplicateUser(User user){
+        if(userRepository.findByEmail(user.getEmail())!=null){
+            System.out.println("이미 가입이 된 아이디입니다.");
+        }
+
+    }
+
+    public List<User> findUsers(){
+        return userRepository.findAll();
+    }
+
+
+    public User findOne(String userMail){
+        return userRepository.findByEmail(userMail);
     }
 
 
