a52bddadfbfae756675576706d5370d9ab55cd18
yoonyerin <yerinyoon@ewhain.net>
2022-11-06 18:34:27 +0900

    Fix:delete  blank test-code

--- src/test/java/com/gamjaring/web/springboot/ApplicationTest.java
+++ src/test/java/com/gamjaring/web/springboot/ApplicationTest.java
@@ -1,12 +1,10 @@
-package com.gamjaring.web.springboot;
+/*package com.gamjaring.web.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ApplicationTest {
-    @Test
-    void contextLoads() {
-    }
-
+  
 }
+*/
