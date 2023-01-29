087cce7d33a8336b8d52f5bbb36f2ffd35401b13
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-01-29 16:40:07 +0900

    commit tmp

--- src/main/java/com/gamjaring/web/springboot/service/AmazonS3ResourceStorage.java
+++ src/main/java/com/gamjaring/web/springboot/service/AmazonS3ResourceStorage.java
@@ -3,6 +3,7 @@ package com.gamjaring.web.springboot.service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -35,3 +36,4 @@ public class AmazonS3ResourceStorage {
         }
     }
 }
+
